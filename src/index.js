a68af793262722ede9559905173f1eb105a1ce4e
Виталий <101243525+Vit-all-in@users.noreply.github.com>
2023-02-20 10:36:32 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function toReadable (number) {
-  
+    let a = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let b = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let c = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let d = [Math.trunc(number / 100), Math.trunc(number % 100 / 10), number % 10];
+    let result = "";
+
+    if (d[0]) result += `${a[d[0] - 1] + " hundred "}`;
+    if (d[1] > 1) result += c[d[1] - 2] + " ";
+    if (d[1] === 1) result += b[d[2]];
+    if (d[1] !== 1 && d[2]) result += a[d[2] - 1];
+    if (result[result.length - 1] == " ") result = result.slice(0, -1);
+    if (result === "") result += "zero";
+
+    return result;
 }
